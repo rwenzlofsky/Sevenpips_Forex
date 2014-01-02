5f4f45dbaf7c45045c1e96416b95b2a28461deb7
rwenzlofsky <r.wenzlofsky@gmail.com>
2014-01-02 23:24:29 +0100

    Initial

---
+++ Sevenpips/Sevenpips.h
@@ -0,0 +1,27 @@
+// Sevenpips.h : Hauptheaderdatei für die Sevenpips-DLL
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "'stdafx.h' vor dieser Datei für PCH einschließen"
+#endif
+
+#include "resource.h"		// Hauptsymbole
+
+
+// CSevenpipsApp
+// Siehe Sevenpips.cpp für die Implementierung dieser Klasse
+//
+
+class CSevenpipsApp : public CWinApp
+{
+public:
+	CSevenpipsApp();
+
+// Überschreibungen
+public:
+	virtual BOOL InitInstance();
+
+	DECLARE_MESSAGE_MAP()
+};
